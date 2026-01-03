d37eb42ea6d71c3522f9d18a6c6f1e62d49afe47
markstrayer1 <strayer.mark@gmail.com>
2026-01-03 13:35:37 +0000

    Deploying to gh-pages from @ markstrayer1/markstrayer1.github.io@105fb9353f812f380eaaff516ea52799b4dacb29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
+  },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-teaching",
           title: "teaching",
           description: "",
           section: "Navigation",
